39474b4a80660bea6c5b9fee14dd5e527cf9d42d
Igor Nascimento <nigor6803@gmail.com>
2024-04-19 21:55:39 -0300

    cliente

---
+++ src/aplicacao/Progama.java
@@ -0,0 +1,18 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Progama {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner edd = new Scanner(System.in);
+		
+		
+		
+		edd.close();
+	}
+
+}
---
+++ src/entidade/Cliente.java
@@ -0,0 +1,47 @@
+package entidade;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Cliente {
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	private String nome;
+	private String email;
+	private Date diaDoAniversario;
+		
+	private Cliente() {
+		}
+	
+	private Cliente (String nome,String email,Date diaDoAniversario) {
+		this.nome = nome;
+		this.email = email;
+		this.diaDoAniversario = diaDoAniversario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDiaDoAniversario() {
+		return diaDoAniversario;
+	}
+
+	public void setDiaDoAniversario(Date diaDoAniversario) {
+		this.diaDoAniversario = diaDoAniversario;
+	}
+	
+	
+}
